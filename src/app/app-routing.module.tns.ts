5e2da5c98fe6e884a3c8d265cb426dfd3316dca9
sebawita <sebawita@gmail.com>
2018-10-10 16:59:53 +0100

    migrated app

---
+++ src/app/app-routing.module.tns.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from './messages/messages.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'messages', component: MessagesComponent },
+];
+
+@NgModule({
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/drawer-action-bar/drawer-action-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import * as app from 'tns-core-modules/application';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+
+@Component({
+  selector: 'drawer-action-bar',
+  templateUrl: './drawer-action-bar.component.html',
+  styleUrls: ['./drawer-action-bar.component.css']
+})
+export class DrawerActionBarComponent {
+  @Input() title = '';
+
+  showDrawer() {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+  }
+}
